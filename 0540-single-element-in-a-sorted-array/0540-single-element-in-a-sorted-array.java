cd6ad71306e5279ebc0edc186d71313f4199e4bc
VISH__01 <98100559+TECH9PATH-VISH@users.noreply.github.com>
2026-09-03 14:42:30 +0530

    Time: 0 ms (100%), Space: 53 MB (21.89%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,10 +1,21 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int x=0;
-        for(int n: nums){
-            x^=n;
+        // 1hr 28m 57s
+        int i=0;
+        int j=nums.length-1;
+        while(i<j){
+            int mid=(i+((j-i)/2));
+            if(mid%2==1){
+                mid-=1;
+            }
+            if(nums[mid]==nums[mid+1]){
+                i=mid+2;
+            }
+            else{
+                j=mid;
+            }
         }
-        return x;
+        return nums[j];
         
     }
 }
