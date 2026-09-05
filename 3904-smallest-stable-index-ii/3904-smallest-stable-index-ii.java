adcb33f1c0f2c7dedfb5f1884a60693ce4061293
VISH__01 <98100559+TECH9PATH-VISH@users.noreply.github.com>
2026-09-05 14:28:50 +0530

    Time: 7 ms (10.69%), Space: 126.7 MB (59.92%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        if(nums.length==1){
+            return 0;
+        }
+        int[] max = new int[nums.length];
+        int[] min = new int[nums.length];
+        int i=0;
+        int j=1;
+        int a=1;
+        max[0] = nums[0];
+        while(j<nums.length){
+            if(nums[i]>nums[j]){
+                max[a]=nums[i];
+                a++;
+                j++;
+            }else{
+                max[a]=nums[j];
+                a++;
+                i=j;
+                j++;
+            }
+        }
+        i=nums.length-1;
+        j=nums.length-2;
+        int b=nums.length-2;
+        min[nums.length-1]=nums[nums.length-1];
+        while(j>=0){
+            if(nums[i]<nums[j]){
+                min[b]=nums[i];
+                b--;
+                j--;
+            }else{
+                min[b]=nums[j];
+                b--;
+                i=j;
+                j--;
+            }
+        }
+        
+
+        i=0;
+        for(i=0;i<nums.length;i++){
+            int diff=max[i]-min[i];
+            if(diff<=k){
+                return i;
+            }
+        }
+        return -1;
+        
+        
+
+    }
+}
