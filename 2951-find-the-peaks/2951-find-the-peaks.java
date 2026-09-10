6fb166e725493e9f7b5fe3c99ff5d2f797cc6e0a
VISH__01 <98100559+TECH9PATH-VISH@users.noreply.github.com>
2026-09-10 17:08:25 +0530

    Time: 1 ms (99.63%), Space: 45.4 MB (55.64%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.java
+++ 2951-find-the-peaks/2951-find-the-peaks.java
@@ -7,6 +7,7 @@ class Solution {
         for(int i=1;i<mountain.length-1;i++){
             if(mountain[i]>mountain[i-1] && mountain[i]>mountain[i+1]){
                 sc.add(i);
+                i++;
             }
         }
         return sc;
