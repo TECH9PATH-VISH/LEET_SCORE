27a6b7b2940c28d2dc7e7730d1b02c1634062882
VISH__01 <98100559+TECH9PATH-VISH@users.noreply.github.com>
2026-09-10 17:07:45 +0530

    Time: 1 ms (99.63%), Space: 45.6 MB (34.35%) - LeetHub

---
+++ 2951-find-the-peaks/2951-find-the-peaks.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> findPeaks(int[] mountain) {
+        List<Integer> sc = new ArrayList<>();
+        if(mountain.length<3){
+            return sc;
+        }
+        for(int i=1;i<mountain.length-1;i++){
+            if(mountain[i]>mountain[i-1] && mountain[i]>mountain[i+1]){
+                sc.add(i);
+            }
+        }
+        return sc;
+        
+    }
+}
